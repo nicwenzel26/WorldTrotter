87eda5da77c1ff21ac0aaba937b7c722f2f5b990
nicwenzel26 <nwenzel@mymail.mines.edu>
2021-06-25 09:56:45 -0600

    Completed chapter 3, added 5 text views and defined constraints using auto layout system

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        /* Programic definition of views example
+        
+        // Initializing a frame with origin at (160, 240) with a width of 100 points (not px) and height of 150 points
+        // CGRect means colored rectangle
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        // Initializing a view with the previously defined frame
+        let firstView = UIView(frame: firstFrame)
+        
+        // Setting the background color of the view to be blue
+        firstView.backgroundColor = UIColor.blue
+        
+        // Adding the blue rectangle as a subveiw of the view controller
+        view.addSubview(firstView)
+        
+        // Creating a rectangle frame with and origin of (20, 30) and a height and width of 50 points
+        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+        // Creating a view using the second frame
+        let secondView = UIView(frame: secondFrame)
+        // Setting the color of the view to be green
+        secondView.backgroundColor = UIColor.green
+        // Adding the second green rectangle as a sub view of the first blue rectangle, changing the greens origin relative to the blue 
+        firstView.addSubview(secondView)
+         */
+        
+        
     }
 
 
