6ac26772a8f05d3d29001d27a42c73eebc38668c
nicwenzel26 <nwenzel@mymail.mines.edu>
2021-06-30 20:34:34 -0600

    Added outlets for the text feilds and labels and added basic logic to update.

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ConversionViewController: UIViewController {
     
+    // Outlet definitions
+    @IBOutlet var celciusLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,8 +24,16 @@ class ConversionViewController: UIViewController {
         // For silver challenge, randomizeing background color everytime the view for the converter is loaded on screen 
         //view.backgroundColor = UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
         
-        
-        
+    }
+    
+    @IBAction func fahrenheitFieldEditingCHanged(_ textField: UITextField) {
+        // If the feild is empty show '???' otherwise show the number
+        if let text = textField.text, !text.isEmpty {
+            celciusLabel.text = text
+        }
+        else {
+            celciusLabel.text = "???"
+        }
     }
     
 }
