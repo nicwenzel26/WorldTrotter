876c454369af474353fffbb6fe187e8b706c4996
nicwenzel26 <nwenzel@mymail.mines.edu>
2021-07-08 18:59:10 -0600

    Succesfully implemented a tap gesture to dismiss keyboard

--- WorldTrotter/LengthConverterViewController.swift
+++ WorldTrotter/LengthConverterViewController.swift
@@ -8,5 +8,25 @@
 import UIKit
 
 class LengthConverterViewController: UIViewController {
+    @IBOutlet var centimetersLabel: UILabel!
+    @IBOutlet var inchesTextField: UITextField!
+    
+    
+    
+    // Function for changing the field
+    @IBAction func inchesFeildChanged(_ textField: UITextField) {
+        // If the field is empty show ???
+        if let text = textField.text, !text.isEmpty {
+            centimetersLabel.text = text
+        }
+        else {
+            centimetersLabel.text = "???"
+        }
+    }
+    
+    // Function for dismissing the keyboard
+    @IBAction func dismissKeyboard(_ sender:UITapGestureRecognizer) {
+        inchesTextField.resignFirstResponder()
+    }
     
 }
